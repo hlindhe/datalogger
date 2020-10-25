25650aa95d75824ac26965a68fa8b54307c17155
Henrik Lindhe <henrik.lindhe@gmail.com>
2020-10-25 23:21:58 +0100

    MQTT support

--- seriallogger.py
+++ seriallogger.py
@@ -6,11 +6,22 @@ import pprint
 import string
 import json
 import os
-
+import paho.mqtt.client as mqtt
 import socket
 
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
 running = True
+
+
+mqttclient = mqtt.Client()
+mqttclient.connect("localhost")
+mqttclient.loop_start()
+
 ser = serial.Serial('/dev/ttyUSB0',115200)
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -47,39 +58,51 @@ def process_bmp(data):
     kv=dict()
     dv=data.split(",")
     if len(dv)==3:
-        kv['temperature']=float(dv[0])
-        kv['pressure']=float(dv[1])
-        kv['altitude']=float(dv[2])
-        kv['sensor']='bmp180'
-        kv['ts']=time.time()
-        send_bmp(kv)
+        mqttclient.publish("bmp180/temperature",float(dv[0]))
+        mqttclient.publish("bmp180/pressure",float(dv[1]))
+        mqttclient.publish("bmp180/altitude",float(dv[2]))
+#        kv['temperature']=float(dv[0])
+#        kv['pressure']=float(dv[1])
+#        kv['altitude']=float(dv[2])
+#        kv['sensor']='bmp180'
+#        kv['ts']=time.time()
+#        send_bmp(kv)
     return
 
 def process_mpu(data):
     kv=dict()
     dv=data.split(",")
     if len(dv)==7:
-        kv['accelerometerx']=float(dv[0])
-        kv['accelerometery']=float(dv[1])
-        kv['accelerometerz']=float(dv[2])
-        kv['gyroscopex']=float(dv[3])
-        kv['gyroscopey']=float(dv[4])
-        kv['gyroscopez']=float(dv[5])
-        kv['temperature']=float(dv[6])
-        kv['sensor']='mpu6050'
-        kv['ts']=time.time()
-        send_mpu(kv)
+        mqttclient.publish("mpu6050/accelerometer_x",float(dv[0]))
+        mqttclient.publish("mpu6050/accelerometer_y",float(dv[1]))
+        mqttclient.publish("mpu6050/accelerometer_z",float(dv[2]))
+        mqttclient.publish("mpu6050/gyroscope_x",float(dv[3]))
+        mqttclient.publish("mpu6050/gyroscope_y",float(dv[4]))
+        mqttclient.publish("mpu6050/gyroscope_z",float(dv[5]))
+        mqttclient.publish("mpu6050/temperature",float(dv[6]))
+#        kv['accelerometerx']=float(dv[0])
+#        kv['accelerometery']=float(dv[1])
+#        kv['accelerometerz']=float(dv[2])
+#        kv['gyroscopex']=float(dv[3])
+#        kv['gyroscopey']=float(dv[4])
+#        kv['gyroscopez']=float(dv[5])
+#        kv['temperature']=float(dv[6])
+#        kv['sensor']='mpu6050'
+#        kv['ts']=time.time()
+#        send_mpu(kv)
     return
 
 def process_mpl(data):
     kv=dict()
     dv=data.split(",")
     if len(dv)==2:
-        kv['altitude']=float(dv[0])
-        kv['temperature']=float(dv[1])
-        kv['sensor']='mpl3115a2'
-        kv['ts']=time.time()
-        send_mpl(kv)
+        mqttclient.publish("mpl3115a2/altitude",float(dv[0]))
+        mqttclient.publish("mpl3115a2/temperature",float(dv[1]))
+#        kv['altitude']=float(dv[0])
+#        kv['temperature']=float(dv[1])
+#        kv['sensor']='mpl3115a2'
+#        kv['ts']=time.time()
+#        send_mpl(kv)
     return
 
 def process_wifi(data):
@@ -87,17 +110,21 @@ def process_wifi(data):
     wv=dict()
     i=0
     for s in range(int(len(dv)/5)):
-        kv = dict()        
-        kv['ssid']=dv[s*5]
-        kv['rssi']=int(dv[s*5+1])
-        kv['bssid']=dv[s*5+2]
-        kv['channel']=dv[s*5+3]
-        kv['encryption']=dv[s*5+4]
-        kv['sensor']='esp8266wifi'
-        kv['ts']=time.time()
-        wv[i]=kv
-        i=i+1
-    send_wifi(wv)
+        mqttclient.publish("esp8266wifi/"+dv[s*5+2]+"/ssid",dv[s*5])
+        mqttclient.publish("esp8266wifi/"+dv[s*5+2]+"/rssi",dv[s*5+1])
+        mqttclient.publish("esp8266wifi/"+dv[s*5+2]+"/channel",dv[s*5+3])
+        mqttclient.publish("esp8266wifi/"+dv[s*5+2]+"/encryption",dv[s*5+4])
+#        kv = dict()        
+#        kv['ssid']=dv[s*5]
+#        kv['rssi']=int(dv[s*5+1])
+#        kv['bssid']=dv[s*5+2]
+#        kv['channel']=dv[s*5+3]
+#        kv['encryption']=dv[s*5+4]
+#        kv['sensor']='esp8266wifi'
+#        kv['ts']=time.time()
+#        wv[i]=kv
+#        i=i+1
+#    send_wifi(wv)
     return
 
 
